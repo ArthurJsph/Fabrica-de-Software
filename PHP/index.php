bc10903b97d9ab838cc8f342b4c1a94a1521e79b
Arthur José <jarthurben10@gmail.com>
2024-11-18 08:58:23 -0300

    Estrutura Base

---
+++ PHP/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabrica de Software</title>
+    <link rel="stylesheet" href="../../css/form.css">
+</head>
+<body>
+    <h1>Fabrica de Software</h1>
+                      
+</body>
+</html>
